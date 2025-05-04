10fcb50401df540217bdd1396adb7fae7d6991ca
Oğuzhan Kertmen <iletisim@oguzhankertmen.com.tr>
2025-05-04 22:46:03 +0300

    Change config

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,5 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server"
 
+export const dynamic = 'force-static'
+
 // Blog yazılarını getir
 export async function GET(request: NextRequest) {
   try {
